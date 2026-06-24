0a6dc4535c1e4a88b183062c64cc5b5b1929330e
Farwah Hamid <farwah.h1@gmail.com>
2026-06-24 14:00:09 +0500

    Add files via upload

---
+++ PhantomnetUI/src/components/ui/index.ts
@@ -0,0 +1,12 @@
+// Re-export all UI components for easier imports
+export * from "./alert"
+export * from "./badge"
+export * from "./button"
+export * from "./card"
+export * from "./dialog"
+export * from "./dropdown-menu"
+export * from "./input"
+export * from "./label"
+export * from "./progress"
+export * from "./select"
+export * from "./tabs"
